66a55d96adb2e0dd3cbdf66ec73f2a993499d9fc
Nadia <hope.nadzeya@gmail.com>
2026-02-14 15:43:25 +0100

    Places app: setup project

---
+++ places/places/placesApp.swift
@@ -0,0 +1,17 @@
+//
+//  placesApp.swift
+//  places
+//
+//  Created by Nadia Ivanovskaya on 14.02.26.
+//
+
+import SwiftUI
+
+@main
+struct placesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
